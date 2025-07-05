bd828973d535f04efa590b3f73379edbd99f2375
Prathapb003 <prathapbalakumar@gmail.com>
2025-07-05 20:50:51 +0530

    Add files via upload

--- palindrome.java
+++ palindrome.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-public class sum {
+public class palindrome {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int sum=0,a; 
-        for(;n>0;){
+        while (n>0) {
             a=n%10;
-            sum+=a;
+            sum=sum*10+a;
             n=n/10;
         }System.out.println(sum);
     }
-}
 
+}
